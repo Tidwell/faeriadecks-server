188faccf6b567ac82a4753705008d98a65f6fbd4
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-10-02 11:54:02 -0400

    env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var env = require('./env.js')
 var scraper = require('./scrape.js');
 scraper.scrape();
 
@@ -6,10 +7,24 @@ var express = require("express");
 var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname+'/../faeria-deckbuilder/app'));
+var dir, indexRoute;
+if (env === 'local') {
+	dir = '/../faeria-deckbuilder/app';
+	indexRoute = '/../faeria-deckbuilder/app/index.html';
+}
+if (env === 'stage') {
+	dir = '/../faeria-deckbuilder/dist';
+	indexRoute = '/../faeria-deckbuilder/dist/index.html';
+}
+if (env === 'prod') {
+	dir = '/../faeriadecks/';
+	indexRoute = '/../faeriadecks/index.html';
+}
+
+app.use(express.static(__dirname+dir));
 
 app.listen(9005, "127.0.0.1");
 
 app.get('/*', function(req,res) {
-	res.sendfile(path.normalize(__dirname + '/../faeria-deckbuilder/app/index.html'));
+	res.sendfile(path.normalize(__dirname + indexRoute));
 });
